4c454172226b1add94436a37ee5f56f2c7c5d7eb
chachacha912 <dkwkdnwk@naver.com>
2021-03-26 14:01:32 +0900

    dll 수정

--- 0323/doubly_linked_list.py
+++ 0323/doubly_linked_list.py
@@ -4,84 +4,98 @@ class Node:
         self.next = next
         self.prev = prev
 
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
     
     def is_empty(self):
-        if self.head == None:
-            return True
-        return False
+        return self.head == None
 
     def prepend(self, value):
-        if self.head == None:
+        if self.head is None:
             self.head = Node(value, None, None)
             self.tail = self.head
         else:
-            node = Node(value, self.head, None)
-            self.head.prev = node
-            self.head = node
+            self.head.prev = Node(value, self.head, None)
+            self.head = self.head.prev
 
     def append(self, value):
-        if self.head == None:
+        if self.head is None:
             self.head = Node(value, None, None)
             self.tail = self.head
         else:
-            node = Node(value, None, self.tail)
-            self.tail.next = node
-            self.tail = node
+            self.tail.next = Node(value, None, self.tail)
+            self.tail = self.tail.next
 
     def set_head(self, index):
-        if self.head == None:
+        if self.head is None:
             return False
-        
-        node = self.head
+
+        prev_node = self.head
         for i in range(index):
-            if node.next == None:
+            if prev_node is None:
                 return False
-            node = node.next
+            prev_node = prev_node.next 
         
-        self.head = node
+        self.head = prev_node
         self.head.prev = None
+
         return True
 
-    def access(self, index):
-        if self.head == None:
-            return None
-        
-        node = self.head
-        for i in range(index):
-            if node.next == None:
-                return None
-            node = node.next
-        
-        if node == None:
-            return None
+    def set_tail(self, index):
+        if DoublyLinkedList.is_empty(self):
+            print('SSL is empty!')
         else:
-            return node.value
+            node = self.head
+            idx = 0
+            while node:
+                if idx == index:
+                    self.tail = node
+                    self.tail.next = None
+                    return
+                node = node.next
+                idx += 1
+            print('Invalid index check the size of SSL')
+
+    def access(self, index):
+        if self.head is None:
+            return None
+
+        curr_node = self.head
+        for i in range(index):
+            if curr_node.next is None:
+                print('dd2')
+                return None
+            curr_node = curr_node.next
+            
+        return curr_node.value
+
 
     def insert(self, index, value):
-        if self.head == None and index > 0:
+
+        if self.head is None and index > 0:
             return False
 
         if index == 0:
             self.prepend(value)
             return True
-        
-        node = self.head
-        for i in range(index):
-            if node.next == None:
-                return False
-            node = node.next
-        
-        if node == None:
-            node = Node(value, None, self.tail)
-            self.tail.next = node
-            self.tail = node
         else:
-            node = Node(value, node, node.prev)
-            node.prev.next = node
+            curr_node = self.head
+            for i in range(index):
+                if curr_node.next is None:
+                    if i == index-1:
+                        curr_node = Node(value, None, self.tail)
+                        self.tail.next = curr_node
+                        self.tail = curr_node
+                        return True
+                    else:
+                        return False
+                curr_node = curr_node.next 
+        new_Node = Node(value, curr_node, curr_node.prev)
+        curr_node.prev.next = new_Node
+        curr_node.prev= new_Node
         return True
 
     def remove(self, index):
@@ -96,7 +110,8 @@ class DoublyLinkedList:
         
         node = self.head
         for i in range(index):
-            if node.next == None:
+            print(i)
+            if node is None:
                 return False
             node = node.next
         
@@ -105,21 +120,24 @@ class DoublyLinkedList:
         else:
             node.prev.next = node.next
             if node.next:
+                print(111)
                 node.next.prev = node.prev
             else:
+                print(222)
                 self.tail = node.prev
+            print(333)
             return True
 
     def print(self):
-        if self.head == None:
+        if self.head is None:
             print('[]')
             return
-
-        node = self.head
-        while node.next:
-            print(node.value, end=' ')
-            node = node.next
-        print(node.value)
+        else:
+            node = self.head
+            while node:
+                print(node.value, end = ' ')
+                node = node.next
+            print()
 
     def print_reverse(self):
         if self.head == None:
@@ -131,7 +149,6 @@ class DoublyLinkedList:
             print(node.value, end=' ')
             node = node.prev
         print(node.value)
-
 
 dll = DoublyLinkedList()
 print('\n0. print')
@@ -145,12 +162,13 @@ dll.prepend(1)
 dll.prepend(2)
 dll.print()
 
+
+
 print('\n3. append')
 for i in range(5):
     dll.append(i+3)
 dll.print()
-
-print('\n4. print_reverse')
+print('reverse')
 dll.print_reverse()
 
 print('\n5. set_head')
@@ -158,15 +176,44 @@ print(dll.set_head(3))
 dll.print()
 print(dll.set_head(11))
 dll.print()
+print('reverse')
+dll.print_reverse()
 
 print('\n6. access')
 print(dll.access(0))
-print(dll.access(5))
+print(dll.access(4))
 
 print('\n7. insert')
 print(dll.insert(0,1))
 dll.print()
+dll.print_reverse()
+print('--------------------------')
+print(dll.insert(1,10))
+dll.print()
+dll.print_reverse()
+print('--------------------------')
+print(dll.insert(3,11))
+dll.print()
+dll.print_reverse()
+print('--------------------------')
+print('reverse')
+dll.print_reverse()
 
 print('\n8. remove')
 print(dll.remove(0))
 dll.print()
+dll.print_reverse()
+print('----')
+print(dll.remove(4))
+dll.print()
+dll.print_reverse()
+print('----')
+print(dll.remove(3))
+dll.print()
+dll.print_reverse()
+print('----')
+print(dll.remove(9))
+dll.print()
+
+print('----')
+dll.print_reverse()
